89f5670363ce0fa70eea1c72ff764392541128f7
vigkast <vigneshkasthuri2009@gmail.com>
2016-02-13 12:11:59 +0530

    change

--- js/controllers.js
+++ js/controllers.js
@@ -343,44 +343,43 @@ $scope.slide = [];
   $scope.menutitle = NavigationService.makeactive("Brands");
   TemplateService.title = $scope.menutitle;
   $scope.navigation = NavigationService.getnav();
-  $scope.brandlist = [{
-    img: 'img/brands/18.png'
-
-  }, {
-    img: 'img/brands/1.png'
-  }, {
-    img: 'img/brands/2.png'
-  }, {
-    img: 'img/brands/3.png'
-  }, {
-    img: 'img/brands/4.png'
-  }, {
-    img: 'img/brands/5.png'
-  }, {
-    img: 'img/brands/6.png'
-  }, {
-    img: 'img/brands/7.png'
-  }, {
-    img: 'img/brands/8.png'
-  }, {
-    img: 'img/brands/9.png'
-  }, {
-    img: 'img/brands/10.png'
-  }, {
-    img: 'img/brands/11.png'
-  }, {
-    img: 'img/brands/12.png'
-  }, {
-    img: 'img/brands/13.png'
-  }, {
-    img: 'img/brands/14.png'
-  }, {
-    img: 'img/brands/15.png'
-  }, {
-    img: 'img/brands/16.png'
-  }, {
-    img: 'img/brands/17.png'
-  }];
+  // $scope.brandlist = [{
+  //   img: 'img/brands/18.png'
+  // }, {
+  //   img: 'img/brands/1.png'
+  // }, {
+  //   img: 'img/brands/2.png'
+  // }, {
+  //   img: 'img/brands/3.png'
+  // }, {
+  //   img: 'img/brands/4.png'
+  // }, {
+  //   img: 'img/brands/5.png'
+  // }, {
+  //   img: 'img/brands/6.png'
+  // }, {
+  //   img: 'img/brands/7.png'
+  // }, {
+  //   img: 'img/brands/8.png'
+  // }, {
+  //   img: 'img/brands/9.png'
+  // }, {
+  //   img: 'img/brands/10.png'
+  // }, {
+  //   img: 'img/brands/11.png'
+  // }, {
+  //   img: 'img/brands/12.png'
+  // }, {
+  //   img: 'img/brands/13.png'
+  // }, {
+  //   img: 'img/brands/14.png'
+  // }, {
+  //   img: 'img/brands/15.png'
+  // }, {
+  //   img: 'img/brands/16.png'
+  // }, {
+  //   img: 'img/brands/17.png'
+  // }];
 })
 .controller('KnowusCtrl', function($scope, TemplateService, NavigationService, ngDialog, $timeout) {
   $scope.template = TemplateService.changecontent("knowus");
